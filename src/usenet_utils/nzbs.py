6fa7c8052a6639b893983580979a5d979d4cebd6
Sam Rogers <sjr@samrogers.org>
2018-06-12 18:29:55 -0400

    basic nzb parsing

---
+++ src/usenet_utils/nzbs.py
@@ -0,0 +1,51 @@
+import os
+from lxml import objectify
+
+def scan_folder(dir_path):
+    nzb_dir = os.path.realpath(dir_path)
+    nzbs = [f for f in os.listdir(nzb_dir) if f.endswith(".nzb")]
+    for nzb in nzbs:
+        yield os.path.join(nzb_dir, nzb)
+
+class FileTarget:
+    groups = []
+    segments = []
+
+    def __init__(self, elem):
+        self._elem = elem
+
+    @property
+    def attrib(self):
+        return self._elem.attrib
+
+    def __repr__(self):
+        fmt_str = "<FileTarget: {} groups, {} segments>"
+        return fmt_str.format(len(self.groups),
+                              len(self.segments))
+
+class NzbFile(object):
+
+    def __init__(self, nzb_path):
+        self.nzb_path = os.path.realpath(nzb_path)
+        self.files = []
+
+        self.load()
+
+    def load(self):
+        tree = objectify.parse(self.nzb_path)
+        root = tree.getroot()
+        dtd_namespace = "".join(['{', root.nsmap[None], '}'])
+
+        # TODO: extract <HEAD> data
+
+        # extract files
+        file_mask = dtd_namespace + "file"
+        for f in root.findall(file_mask):
+            target = FileTarget(elem = f)
+            groups, segments = f.getchildren()
+            target.groups = [g for g in groups.getchildren()]
+            target.segments = [s for s in segments.getchildren()]
+            self.files.append( target )
+
+    def __repr__(self):
+        return os.path.basename(self.nzb_path)
